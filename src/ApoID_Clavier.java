bd05fff5089effbd9e33a96ab96f3b1b3b10b51d
david-michael.crvlh <david-michael.crvlh@eduge.ch>
2022-12-05 16:47:08 +0100

    Changement type input du code, d'une liste à une chaîne de caractères. Verification code commencée

--- src/ApoID_Clavier.java
+++ src/ApoID_Clavier.java
@@ -10,12 +10,16 @@ public class ApoID_Clavier extends ApoDialog implements ActionListener {
     private static final int LONGUEUR_CODE = 4;
     private String resultatPourAffichage = super.retournerResultat();
     private JButton[] bouton = new JButton[10];
-    ArrayList<Integer> inputCode = new ArrayList<>();
+    private String inputCode = "";
     private JButton[] boutonDisabled = new JButton[5];
+    private JButton go;
+    private JButton stop;
+    private String code;
 
     ApoID_Clavier(JFrame f, String titre, String codeCorrect)// NE PAS MODIFIER  cet en-tête
     {
         super(f, titre);
+        code = codeCorrect;
         setLayout(new GridLayout(4, 0));
         for (int i = 0; i < CHIFFRES_DANS_L_ORDRE.length(); i++) {
             bouton[i] = new JButton();
@@ -25,16 +29,19 @@ public class ApoID_Clavier extends ApoDialog implements ActionListener {
         }
 
 
-        JButton go = new JButton("Go");
+        go = new JButton("Go");
+        go.addActionListener(this);
         go.setBackground(Color.BLACK);
         go.setForeground(Color.ORANGE);
 
-        JButton stop = new JButton("Stop");
+        stop = new JButton("Stop");
+        stop.addActionListener(this);
         stop.setBackground(Color.BLACK);
         stop.setForeground(Color.ORANGE);
 
         add(go);
         add(stop);
+
         for (int i = 0; i < 4; i++) {
             boutonDisabled[i] = new JButton();
             add(boutonDisabled[i]);
@@ -53,12 +60,19 @@ public class ApoID_Clavier extends ApoDialog implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
         for (int i = 0; i < CHIFFRES_DANS_L_ORDRE.length(); i++) {
             if (ae.getSource().equals(bouton[i])) {
-                inputCode.add(i);
-                for (int j = 0; j < inputCode.size(); j++) {
-                    if (inputCode.size() <= LONGUEUR_CODE){
+                inputCode += String.valueOf(i);
+                for (int j = 0; j < inputCode.length(); j++) {
+                    if (inputCode.length() <= LONGUEUR_CODE) {
                         boutonDisabled[j].setText("_");
                     }
                 }
+            }
+        }
+        if (ae.getSource().equals(go)) {
+            if (code.equals(inputCode)){
+                System.out.println("OK");
+            } else {
+                System.out.println("Pas Ok");
             }
         }
 
